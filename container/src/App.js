2073a2d5f11a9bb45aeae0e0761c65fa7198cff8
Thien Nguyen <mikenguyen@Mikes-MacBook-Air.local>
2021-06-06 15:32:28 +0700

    init commit

---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+    return (
+        <div>
+            fafsfs
+            <hr/>
+            <MarketingApp />
+        </div>
+    )
+}
